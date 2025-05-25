e979d382d4955342d45d0967b9ebceacc41d468a
honoka <mintbeer0924@gmail.com>
2025-05-25 18:16:59 +0900

    メール認証のCSS修正

--- src/resources/views/verify_email.blade.php
+++ src/resources/views/verify_email.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('/css/verify.css')  }}">
+<link rel="stylesheet" href="{{ asset('css/verify.css')  }}">
 @endsection
 
 @section('content')
 <div class="verification-container">
     <div class="verification-content">
-        <p class="verification__message">
+        <p class="verification-content__message">
             登録していただいたメールアドレスに認証メールを送付しました。<br>
             メール認証を完了してください。
         </p>
     </div>
 
-    <div class="verification__button">
-        <a href="https://mailtrap.io/home" class="verification__button">
+    <div class="verification-btn">
+        <a href="https://mailtrap.io/home" class="verification-btn__link">
             認証はこちらから
         </a>
     </div>
@@ -22,7 +22,7 @@
     <div class="verification-resend">
         <form class="resend-form" method="POST" action="{{ route('verification.send') }}">
             @csrf
-            <button type="submit" class="resend__button">
+            <button type="submit" class="resend-form__btn">
                 認証メールを再送する
             </button>
         </form>
