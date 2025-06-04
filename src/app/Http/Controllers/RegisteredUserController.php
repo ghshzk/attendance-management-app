af72b8c339b6eed9e6e20805c3f091d35eaf49b2
honoka <mintbeer0924@gmail.com>
2025-06-04 09:41:29 +0900

    レスポンシブデザイン修正

--- src/app/Http/Controllers/RegisteredUserController.php
+++ src/app/Http/Controllers/RegisteredUserController.php
@@ -9,7 +9,6 @@ use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
-//use Laravel\Fortify\Contracts\RegisterResponse;
 use Laravel\Fortify\Contracts\RegisterViewResponse;
 use Laravel\Fortify\Fortify;
 use App\Http\Requests\RegisterRequest;
@@ -29,12 +28,9 @@ class RegisteredUserController extends Controller
         $validated = $request->validated();
 
         event(new Registered($user = $creator->create($validated)));
-        //event(new Registered($user = $creator->create($request->all())));
 
         //未認証ユーザー情報をセッションに保存
         session(['unauthenticated_user' => $user]);
-
-        //$this->guard->login($user); //メール認証が完了していないユーザーをログインさせないため削除
 
         return app(RegisterResponse::class);
     }
