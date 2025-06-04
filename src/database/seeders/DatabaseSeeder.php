79f912a8398af88952120a196140ee2257374ba3
honoka <mintbeer0924@gmail.com>
2025-06-04 18:38:20 +0900

    シーダー修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -56,37 +56,6 @@ class DatabaseSeeder extends Seeder
                 ]);
                 BreakTime::factory()->forAttendance($attendance)->create();
             }
-            /*$dates = collect();
-
-            for ($i = 0; $i < 10; $i++) {
-                do {
-                    $date = now()->startOfMonth()->addDays(rand(0, now()->day -1))->format('Y-m-d');
-                } while ($dates->contains($date));
-
-                $dates->push($date);
-
-                $attendance = Attendance::factory()->create([
-                    'user_id' => $user->id,
-                    'date' => $date,
-                ]);
-
-                BreakTime::factory()->forAttendance($attendance)->create();
-            }
-
-            for ($i = 0; $i < 10; $i++) {
-                do {
-                    $date = now()->subMonth()->startOfMonth()->addDays(rand(0, now()->subMonth()->daysInMonth - 1))->format('Y-m-d');
-                } while ($dates->contains($date));
-
-                $dates->push($date);
-
-                $attendance = Attendance::factory()->create([
-                    'user_id' => $user->id,
-                    'date' => $date
-                ]);
-
-                BreakTime::factory()->forAttendance($attendance)->create();
-            }*/
         }
     }
 }
