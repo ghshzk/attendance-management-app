0fcd622cddc07a40657118f409a2ce875b497d86
honoka <mintbeer0924@gmail.com>
2025-06-05 11:43:04 +0900

    CSV機能実装

--- src/app/Http/Controllers/AttendanceListController.php
+++ src/app/Http/Controllers/AttendanceListController.php
@@ -26,32 +26,6 @@ class AttendanceListController extends Controller
                         ->orderBy('date') //日付順で表示
                         ->get();
 
-        $attendances = $attendances->map(function ($attendance) {
-            if ($attendance->clock_in && $attendance->clock_out) {
-                $workTime = Carbon::parse($attendance->clock_in)
-                            ->diffInMinutes(Carbon::parse($attendance->clock_out));
-            } else {
-                $workTime = null;
-            }
-
-            $breakTime = $attendance->breakTimes->reduce(function ($carry, $break) {
-                if ($break->break_start && $break->break_end) {
-                    return $carry + Carbon::parse($break->break_start)
-                    ->diffInMinutes(Carbon::parse($break->break_end));
-                }
-                return $carry;
-            }, 0);
-
-            $actualWorkTime = $workTime !== null ? $workTime - $breakTime : null;
-
-            $attendance->work_time = $actualWorkTime !== null
-                ? sprintf('%d:%02d', floor($actualWorkTime / 60), $actualWorkTime % 60) : null;
-
-            $attendance->break_time = sprintf('%d:%02d', floor($breakTime / 60), $breakTime % 60);
-
-            return $attendance;
-        });
-
         return view('attendance_list', compact('month','attendances'));
     }
 
